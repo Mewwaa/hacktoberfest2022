d335d238de97756f528aeff1a280ac4529353d8d
Dhruvi Agrawal <60813752+dhruviagrawal@users.noreply.github.com>
2022-10-02 17:05:28 +0530

    Update SquareRoot.cpp

---
+++ C++/SquareRoot.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorSqrt(int x)
+{
+    
+    if (x == 0 || x == 1)
+        return x;
+
+    int start = 1, end = x / 2, ans;
+    while (start <= end) {
+        int mid = (start + end) / 2;
+ 
+       
+        int sqr = mid * mid;
+        if (sqr == x)
+            return mid;
+ 
+        if (sqr <= x) {
+            start = mid + 1;
+            ans = mid;
+        }
+        else 
+            end = mid - 1;
+    }
+    return ans;
+}
+ 
+
+int main()
+{
+    int x;
+    cin>>x;
+    cout << floorSqrt(x) << endl;
+    return 0;
+}
